0ffc5484c523404620053b6c0f30e42593acbf2f
AhmadOmran <aomran808@gmail.com>
2022-08-28 12:16:38 +0300

    header and hero section created on desktop

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        openSans: "Open Sans",
+        merriWeather: "Merriweather Sans",
+      },
+    },
+  },
+  plugins: [],
+};
